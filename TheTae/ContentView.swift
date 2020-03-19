69f73c64f7aa90bec67ec6e4de644db3c0b3dd72
Ben <bdl1123@gmail.com>
2020-03-18 21:16:08 -0400

    Initial Commit

---
+++ TheTae/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheTae
+//
+//  Created by Ben Lee on 3/18/20.
+//  Copyright © 2020 Ben Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
